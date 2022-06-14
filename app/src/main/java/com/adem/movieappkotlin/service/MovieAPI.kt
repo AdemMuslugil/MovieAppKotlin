f4ea00b0b2c88dcce17645acbf2342d732f7eddc
Adem Muslugil <Adem.muslugil@hotmail.com>
2022-06-15 00:35:10 +0300

    Security Update 2

--- app/src/main/java/com/adem/movieappkotlin/service/MovieAPI.kt
+++ app/src/main/java/com/adem/movieappkotlin/service/MovieAPI.kt
@@ -8,8 +8,6 @@ import retrofit2.http.Query
 
 interface MovieAPI {
 
-    //https://api.themoviedb.org/3/movie/top_rated?api_key=6b9344e9ba1ce6a689f57a61c0b4450f&language=en-US&page=1
-
     @GET("movie/top_rated")
     fun getMovie(
         @Query("page") page : String,
